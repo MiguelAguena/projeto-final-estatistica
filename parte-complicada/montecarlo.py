5c1a8f71c660b79dc57b67c79de6efd5ef5654d8
MiguelAguena <miguel.aguena@usp.br>
2024-05-23 09:26:59 -0300

    Monte Carlo corrigido

--- parte-complicada/montecarlo.py
+++ parte-complicada/montecarlo.py
@@ -3,38 +3,54 @@ from pathlib import Path
 import statistics as st
 import numpy as np
 
-def NormalMonteCarlo(avg, std_dev, num_reps, sims):
+def normalMonteCarlo(avg, std_dev, num_reps):
     i = 0
-    arr = []
-    while(i < sims):
-        arr.append(np.random.normal(avg, std_dev, num_reps))
-        i = i + 1
-    
+    arr = np.random.normal(avg, std_dev, num_reps)
     return arr
 
 def calcZ(corr):
     return 0.5 * np.log((1 + corr)/(1 - corr))
 
+def calcReturns(data):
+    i = 0
+    returns = []
+    while(i < len(data) - 1):
+        returns.append(np.log(data[i + 1] / data[i]))
+        i = i + 1
+  
+    return returns
+
 def __main__():
     df = pd.read_csv(Path('assets.csv')).dropna()
     amount = len(df.index)
 
-    petr = df['PETR4.SA']
-    wege = df['WEGE3.SA']
-    abev = df['ABEV3.SA']
-    vale = df['VALE3.SA']
+    petr = df['PETR4.SA'].array
+    wege = df['WEGE3.SA'].array
+    abev = df['ABEV3.SA'].array
+    vale = df['VALE3.SA'].array
 
-    petr_var = st.variance(petr)
-    wege_var = st.variance(wege)
-    abev_var = st.variance(abev)
-    vale_var = st.variance(vale)
+    ret_petr  = calcReturns(petr)
+    ret_wege  = calcReturns(wege)
+    ret_abev  = calcReturns(abev)
+    ret_vale  = calcReturns(vale)
 
-    corr_petr_wege = st.correlation(petr, wege)
-    corr_petr_abev = st.correlation(petr, abev)
-    corr_petr_vale = st.correlation(petr, vale)
-    corr_wege_abev = st.correlation(wege, abev)
-    corr_wege_vale = st.correlation(wege, vale)
-    corr_abev_vale = st.correlation(abev, vale)
+    petr_var = st.variance(ret_petr)
+    wege_var = st.variance(ret_wege)
+    abev_var = st.variance(ret_abev)
+    vale_var = st.variance(ret_vale)
+
+    print("Variances")
+    print(petr_var)
+    print(wege_var)
+    print(abev_var)
+    print(vale_var)
+
+    corr_petr_wege = st.correlation(ret_petr, ret_wege)
+    corr_petr_abev = st.correlation(ret_petr, ret_abev)
+    corr_petr_vale = st.correlation(ret_petr, ret_vale)
+    corr_wege_abev = st.correlation(ret_wege, ret_abev)
+    corr_wege_vale = st.correlation(ret_wege, ret_vale)
+    corr_abev_vale = st.correlation(ret_abev, ret_vale)
 
     print("Correlations")
     print(corr_petr_wege)
@@ -63,7 +79,13 @@ def __main__():
     print("Z desvpad")
     print(z_desvpad)
 
-    print("Monte Carlo for PETR - WEGE")
-    print(NormalMonteCarlo(z_petr_wege, z_desvpad, amount, 100))
+    sims = 100
+    print("Monte Carlo")
+    print(normalMonteCarlo(z_petr_wege, z_desvpad, sims))
+    print(normalMonteCarlo(z_petr_abev, z_desvpad, sims))
+    print(normalMonteCarlo(z_petr_vale, z_desvpad, sims))
+    print(normalMonteCarlo(z_wege_abev, z_desvpad, sims))
+    print(normalMonteCarlo(z_wege_vale, z_desvpad, sims))
+    print(normalMonteCarlo(z_abev_vale, z_desvpad, sims))
 
 __main__()
